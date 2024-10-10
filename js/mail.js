ecff0b33f896c04ad2fd70b3672bfaadb9e5148c
HARSH GOYAL <71200531+harshgoyal89@users.noreply.github.com>
2024-10-10 11:39:03 +0530

    Update mail.js

--- js/mail.js
+++ js/mail.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
         };
   
         try {
-          const response = await fetch("http://localhost:3000/send-email", {
+          const response = await fetch("https://JTEnergyPvtLtd.onrender.com/send-email", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
